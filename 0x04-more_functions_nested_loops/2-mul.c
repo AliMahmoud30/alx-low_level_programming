2dff8ba2caa36a94bb276b5a2d5472163dbacea7
AliMahmoud30 <alimahmoudmorsi@gmail.com>
2023-07-21 14:16:15 -0700

    upload 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Check if the given character is an uppercase letter.
+ * @a: number1 to test
+ * @b: number2 to test
+ *
+ * Return: multiplies two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
