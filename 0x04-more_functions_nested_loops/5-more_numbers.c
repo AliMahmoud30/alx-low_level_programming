605522b61e90778c481358cf35efaff717ed36b5
AliMahmoud30 <113010899+AliMahmoud30@users.noreply.github.com>
2023-07-22 21:35:57 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numbers - prints 10 times the numbers,
+ * more_numbers - prints 10 times the numbers.
  *	from 0 to 14, followed by a new line.
  *	only using _putchar twice
  * Return: Always 0 (Success)
