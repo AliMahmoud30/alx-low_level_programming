f593e920c8e0b3d19936d896d78fcf7634555861
AliMahmoud30 <alimahmoudmorsi@gmail.com>
2023-07-22 17:59:00 -0700

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,10 +7,10 @@
 */
 void print_line(int n)
 {
-  while (n > 0)
-    {
-      _putchar('_');
-      n--;
-    }
-  _putchar('\n');
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
 }
