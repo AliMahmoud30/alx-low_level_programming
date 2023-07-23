c36a4fa9a00026e4cbd60f0e31e3a6b0ba3ad372
AliMahmoud30 <113010899+AliMahmoud30@users.noreply.github.com>
2023-07-23 03:53:30 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: is the number of times the character _ should be printed
+ * only using _putchar twice
+ * Return: Always 0 (Success)
+*/
+void print_line(int n)
+{
+  while (n > 0)
+    {
+      _putchar('_');
+      n--;
+    }
+  _putchar('\n');
+}
